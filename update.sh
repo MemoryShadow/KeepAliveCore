c9ff2f295ff06d733ed8dded079a7b0d95eee5e6
yzytmac <yzytmac@163.com>
2022-03-22 11:07:00 +0800

    保活加微信yzytmac

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export https_proxy=http://127.0.0.1:1086 http_proxy=http://127.0.0.1:1086 all_proxy=socks5://127.0.0.1:1080
+cd /Users/yzy/dev/yzy_workspaces/AndroidStudioProjects/KeepAliveCore
+# export https_proxy=http://127.0.0.1:1086 http_proxy=http://127.0.0.1:1086 all_proxy=socks5://127.0.0.1:1080
 touch tag
 git add --all
 git commit -m "保活加微信yzytmac"
