2bf6616c9185fd850d7e387b8cb84843c068d260
yzytmac <yzytmac@163.com>
2022-03-22 10:36:19 +0800

    保活加微信yzytmac

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+export https_proxy=http://127.0.0.1:1086 http_proxy=http://127.0.0.1:1086 all_proxy=socks5://127.0.0.1:1080
 touch tag
 git add --all
 git commit -m "保活加微信yzytmac"
 rm tag
 git add --all
-git commit -m "最强安卓保活"
+git commit -m "保活加微信yzytmac"
 git push origin main
