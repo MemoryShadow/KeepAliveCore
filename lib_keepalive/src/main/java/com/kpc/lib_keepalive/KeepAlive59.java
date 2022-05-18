84c20d901ee071678a7eab9a6296da9d4693a8be
yzytmac <yzytmac@163.com>
2022-05-18 12:56:56 +0800

    2022年 5月18日 星期三 12时56分56秒 CST

---
+++ lib_keepalive/src/main/java/com/kpc/lib_keepalive/KeepAlive59.java
@@ -0,0 +1,5 @@
+public class KeepAlive{
+    public static void main(String[] args) {
+        System.out.println("保活加微信:KeepAliveCore");
+    }
+}
--- lib_keepalive/src/main/java/com/kpc/lib_keepalive/cps.sh
+++ lib_keepalive/src/main/java/com/kpc/lib_keepalive/cps.sh
@@ -2,5 +2,9 @@
 for((i=0;i<100;i++));
 do
     echo $i
-    cp KeepAlive.kt KeepAlive$i.kt
+    cp KeepAlive.java KeepAlive$i.java
+    cp KeepAlive.c KeepAlive$i.c
+    cp KeepAlive.cpp KeepAlive$i.cpp
+    cp KeepAlive.py KeepAlive$i.py
+    cp KeepAlive.js KeepAlive$i.js
 done
